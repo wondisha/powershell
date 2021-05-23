188645f8e0aac315073a6a5a8e47ff91b9ca0b50
wondisha <wondts@gmail.com>
2021-05-22 20:30:36 -0500

    main

--- Monitor.ps1
+++ Monitor.ps1
@@ -1,11 +1,8 @@
-﻿##Import-Module dbachecks, dbatools
-<# $sqlinstances = Get-DbaRegserver -Group SQLCMS
-$testResults = Invoke-DbcCheck -SqlInstance $sqlinstances -Check  DatabaseStatus -PassThru#>
-
-<#$testResults|convert-dbcresult|Write-DbcTable -SqlInstance 'DESKTOP-833QAHA\SQL_01' -Database monitor |
-Select-Object Describe, Context, Name, Result, FailureMessage |
-Export-Csv d:\temp\backups.csv -NoTypeInformation#>
-
+﻿#Declare variable for credential with secure string
+$password = ConvertTo-SecureString 'MySecretPassword' -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ('root', $password)
+#Define registered servers for sql instance
+$sqlinstance = Get-DbaRegserver -Group SQLCMS
 ## Dbachecks Database Connection
 $dbachecksServer = 'DESKTOP-833QAHA\SQL_01'
 $dbachecksDatabase = 'Monitor'
@@ -14,34 +11,7 @@ $dbachecksDatabase = 'Monitor'
 $SqlInstance = Get-DbaRegserver -Group SQLCMS
 $checks = 'sadisabled','autoclose'
  
-Invoke-DbcCheck -SqlInstance $SqlInstance -Checks $checks  -PassThru |
-Convert-DbcResult -Label 'MorningChecks' |
+Invoke-DbcCheck -SqlInstance $SqlInstance -SqlCredential $cred -Checks $checks  -PassThru |
+Convert-DbcResult -Label 'DailyChecks' |
 Write-DbcTable -SqlInstance $dbachecksServer -Database $dbachecksDatabase
 
-$testResults.TestResult|
-Select-Object Context, Name,Describe,Result, FailureMessage |
-Export-Excel @excelsplat -FreezeTopRow -BoldTopRow
-
-$ConditionalFormat =$(
-    New-ConditionalText -Text Failed -Range 'D:D'
-)
-  
-$excelSplat = @{
-    Path               = 'd:\Temp\Backups.xlsx'
-    WorkSheetName      = 'TestResults'
-    TableName          = 'checkresults'
-    Autosize           = $true
-    ConditionalFormat  = $ConditionalFormat
-    IncludePivotTable  = $true
-    PivotRows          = 'Describe'
-    PivotData          = @{Describe='Count'}
-    PivotColumns       = ''
-    IncludePivotChart  = $true
-    ChartType          = 'ColumnStacked'
-}
-  
-
-
-<#Start-DbcPowerBi -FromDatabase   
-
-#####Get-DbaRegServer |Get-DbaDatabase where OWNER -eq sa####>
