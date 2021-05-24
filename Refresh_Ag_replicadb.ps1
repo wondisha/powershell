9e6c06bbfb2bf6eac334d51b1233ecaf24788223
wondisha <wondts@gmail.com>
2021-05-24 10:40:24 -0500

    main

--- Refresh_Ag_replicadb.ps1
+++ Refresh_Ag_replicadb.ps1
@@ -1,26 +1,24 @@
-﻿$server= instance
-$dbname=databasename
-$ProdBackup=Prodbackup
-$PrimaryFbackup =fullbackup
-$primarylbackup=logbackup
-$agname=availabilitygroup
+﻿##Refreshing Availability database##
+#Declare variable for credential with secure string
+$password = ConvertTo-SecureString 'MySecretPassword' -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ('root', $password)
 #Get the primary and secondary replica
-Get-DbaAgReplica -SqlInstance $server
+Get-DbaAgReplica -SqlInstance instance
 #check if the instance is primary
-Get-DbaAvailabilityGroup -SqlInstance $server -IsPrimary
+Get-DbaAvailabilityGroup -SqlInstance instance -SqlCredential $cred -IsPrimary
 #Remove db from primary replica
-Remove-DbaAgDatabase -sqlinstance $server -database $dbname -availabilitygroup $agname
+Remove-DbaAgDatabase -sqlinstance instance -database dbname -availabilitygroup
 
 #Restore the database from latest Prod Backup
 
-Restore-DbaDatabase -SqlInstance $server -DatabaseName $dbname -path $ProdBackup
+Restore-DbaDatabase -SqlInstance instance -DatabaseName dbname -path 'backup path'
 #Take full and log backup of primary
-Backup-DbaDatabase -SqlInstance $server -Database $dbname -path 'backup path.bak' -Type Full
-Backup-DbaDatabase -SqlInstance $Server -Database $dbname -path 'backupa path.trn' -Type Log
+Backup-DbaDatabase -SqlInstance instance -Database dbname -path 'backup path.bak' -Type Full
+Backup-DbaDatabase -SqlInstance instance -Database dbname -path 'backupa path.trn' -Type Log
 
 #Restore Full and Log to the secondary with no recovery
-Restore-DbaDatabase -SqlInstance $server -DatabaseName $dbname -path $PrimaryFbackup -NoRecovery
-Restore-DbaDatabase -SqlInstance $server -DatabaseName $dbname -path $primarylbackup -NoRecovery
+Restore-DbaDatabase -SqlInstance instance -DatabaseName dbname -path 'backup from primary.bak' -NoRecovery
+Restore-DbaDatabase -SqlInstance instance -DatabaseName dbname -path 'back up from primary.trn' -NoRecovery
 #Rejoin Database to availability Group#3
-Add-DbaAgDatabase -SqlInstance $server -AvailabilityGroup $agname -Database dbname -Confirm
-Add-DbaAgDatabase -SqlInstance $server -AvailabilityGroup $agname -Database dbname -confirm
+Add-DbaAgDatabase -SqlInstance instance -AvailabilityGroup agname -Database dbname -Confirm
+Add-DbaAgDatabase -SqlInstance instance -AvailabilityGroup agname -Database dbname -confirm
